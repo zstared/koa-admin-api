1a6d0e84c56d123b1dfc8ec0ad41e9a408a4b144
bruce.zheng <bruce.zheng@wolaidai.com>
2020-01-07 17:13:30 +0800

    update

--- src/config/pro.js
+++ src/config/pro.js
@@ -1,11 +1,22 @@
-const config={
-    
-	port:8083,
+const config = {
 
-	redis_host: '192.168.1.15',
-	redis_port: 6379,
-	redis_password: '',
-	redis_session_db: 1,
+    origin: 'https://www.zhengxinhong.top:8084',
+
+    /**监听端口 */
+    port: 8084,
+
+    /**数据库连接配置 */
+    mysql_host: '127.0.0.1',
+    mysql_port: 3306,
+    mysql_db: 'xinhong_admin',
+    mysql_userid: 'root',
+    mysql_password: 'Abc@123456',
+
+    /**redis连接配置 */
+    redis_host: '127.0.0.1',
+    redis_port: 6379,
+    redis_password: 'dev@123456',
+    redis_session_db: 1,
 };
 
 export default config;
